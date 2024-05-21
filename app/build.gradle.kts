e78edb965bfe79048d11d3e3f6d16fb0eebe9e75
jesicasp <115514246+jesicasp@users.noreply.github.com>
2024-05-22 04:32:51 +0700

    add design dashboard

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -45,4 +48,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //crop profile
+    implementation(libs.circleimageview)
+
+    //slider image
+    implementation ("com.github.denzcoskun:ImageSlideshow:0.1.2")
 }
