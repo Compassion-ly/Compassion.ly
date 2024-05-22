d5123127b3b61d9c1cb519eaae8540a2d68a503e
Ilham Surya Putra Pamungkas <mrlmens01001@gmail.com>
2024-05-22 23:05:13 +0700

    Add screen feature topic histories

---
+++ app/src/main/java/com/capstone/compassionly/presentation/feature/topic_histories/TopicHistoriesActtivity.kt
@@ -0,0 +1,26 @@
+package com.capstone.compassionly.presentation.feature.topic_histories
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.capstone.compassionly.R
+import com.capstone.compassionly.databinding.ActivityTopicHistoriesActtivityBinding
+
+class TopicHistoriesActtivity : AppCompatActivity() {
+    private var _binding : ActivityTopicHistoriesActtivityBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        _binding = ActivityTopicHistoriesActtivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
