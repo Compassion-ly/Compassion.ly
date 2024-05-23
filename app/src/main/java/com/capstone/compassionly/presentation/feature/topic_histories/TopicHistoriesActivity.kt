aa7d08f332f0806efd27643c4cab9e2d8c478faa
Ilham Surya Putra Pamungkas <mrlmens01001@gmail.com>
2024-05-23 12:36:21 +0700

    Completed screen topic histories

--- app/src/main/java/com/capstone/compassionly/presentation/feature/topic_histories/TopicHistoriesActivity.kt
+++ app/src/main/java/com/capstone/compassionly/presentation/feature/topic_histories/TopicHistoriesActivity.kt
@@ -7,16 +7,18 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.capstone.compassionly.R
 import com.capstone.compassionly.databinding.ActivityTopicHistoriesActtivityBinding
+import com.capstone.compassionly.utility.Utils
 
-class TopicHistoriesActtivity : AppCompatActivity() {
+class TopicHistoriesActivity : AppCompatActivity() {
     private var _binding : ActivityTopicHistoriesActtivityBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         _binding = ActivityTopicHistoriesActtivityBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
         setContentView(binding.root)
+        Utils.changeStatusBarColorWhite(this)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
